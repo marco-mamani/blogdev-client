1796933073e4f35e814cee50064836a3c6b866dd
Marco Mamani <natmikhir@gmail.com>
2022-04-23 10:12:45 -0400

    First commit

---
+++ src/components/global/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="text-center bg-light py-4">
+        <h6>Welcome to my blog</h6>
+        <a href="https://duocode-react-resume.web.app/portfolio" target="_blank" rel="noreferrer" className="mb-2 d-block">
+            https://duocode-react-resume.web.app/portfolio
+        </a>
+        <p>Copyright &copy; 2022</p>
+    </div>
+  )
+}
+
+export default Footer
